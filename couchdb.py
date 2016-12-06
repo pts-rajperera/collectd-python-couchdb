2cd66af3545c99e416968169a3631eb8d273fb92
Raj Perera <raj.perera@points.com>
2016-12-06 18:03:46 -0500

    More pythonic string concat

--- couchdb.py
+++ couchdb.py
@@ -27,19 +27,19 @@ def configure_callback(configuration, conf):
 
 def read_callback(configuration):
     for interval in (0, 60, 300, 900):
-        r = requests.get(configuration['url'] + "/_stats?range=" + interval)
+        r = requests.get("{}/_stats?range={}".format(configuration['url'], interval))
         for key, data in r.json().iteritems():
             for subkey, metrics in data.iteritems():
                 for m_type, value in metrics.iteritems():
                     if isinstance(value, numbers.Number):
                         val = collectd.Values(plugin='couchdb', type=_type(key, subkey))
-                        val.plugin_instance = key + "_" + subkey
-                        val.type_instance = m_type + "_" + interval
+                        val.plugin_instance = "{}_{}".format(key, subkey)
+                        val.type_instance = "{}_{}".format(m_type, interval)
                         val.values = [value]
                         val.dispatch()
-    dbs = set(requests.get(configuration['url'] + "/_all_dbs").json())
+    dbs = set(requests.get("{}/_all_dbs".format(configuration['url'])).json())
     for db in dbs ^ set(['_replicator', '_users']):
-        metrics = requests.get(configuration['url'] + "/" + db).json()
+        metrics = requests.get("{}/{}".format(configuration['url'], db)).json()
         for metric in DB_METRICS:
             if metric in metrics:
                 val = collectd.Values(plugin='couchdb', type="gauge", plugin_instance='db_stats')
